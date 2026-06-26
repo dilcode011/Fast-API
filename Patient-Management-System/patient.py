6117891692d2d9aac0aa88abdc1e31b43dcc2a0b
dilcode011 <dilpreetsingh055566@gmail.com>
2026-06-26 19:10:37 +0530

    feat: implement FastAPI endpoints for retrieving and sorting patient records from JSON storage

--- Patient-Management-System/patient.py
+++ Patient-Management-System/patient.py
@@ -1,5 +1,5 @@
 # pyrefly: ignore [missing-import]
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path , Query ,HTTPException
 import json
 
 app= FastAPI()
@@ -29,3 +29,22 @@ def view_by_id(id: str = Path(...,description="Enter the ID of the patient you w
         if patient['id'] == id:
             return patient
     return {"message": "Patient not found"}
+
+
+
+#QUERY PARAMETER
+@app.get('/sort')
+def sort_patients(sort_by:str= Query(...,description='You can sort the records by any one of the following parameters. They are name,age,city,department and consultation fee'),order: str=Query('asc',description='Enter the order in which you want to sort the records. They are asc and desc',enum=['asc','desc'])):
+
+    valid_fields = ['height','weight','bmi']
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400,detail=f'Invalid sort parameter.Please use one of these{valid_fields}')
+    
+    if order not in ['asc','desc']:
+        raise HTTPException(status_code=400,detail=f'Invalid order parameter.Please use one of these{['asc','desc']}')
+    
+    data = load_data()
+    sort_order = True if order == 'desc' else False
+    sorted_Data = sorted(data.values(), key=lambda x: x.get(sort_by, 0), reverse=sort_order)
+    return sorted_Data
