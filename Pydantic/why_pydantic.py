e88585261713e47dd47e53af360f488df1823815
dilcode011 <dilpreetsingh055566@gmail.com>
2026-06-27 14:49:05 +0530

    feat: add sample script demonstrating the need for Pydantic validation in data entry functions

---
+++ Pydantic/why_pydantic.py
@@ -0,0 +1,9 @@
+def insert_patient_data(name:str,age:int):
+    print(name)
+    print(age)
+    print('Inserted into DB ')
+
+insert_patient_data('dilcode',21)
+
+#We need to implement type checking and validation at the time of function call
+#Then we use pydantic 
