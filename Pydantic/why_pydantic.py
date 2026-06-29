a99757318f331ae81d6f407308800cee01630e09
dilcode011 <dilpreetsingh055566@gmail.com>
2026-06-29 17:50:27 +0530

    feat: add demonstration script explaining the benefits of Pydantic for data validation

--- Pydantic/why_pydantic.py
+++ Pydantic/why_pydantic.py
@@ -7,3 +7,10 @@ insert_patient_data('dilcode',21)
 
 #We need to implement type checking and validation at the time of function call
 #Then we use pydantic 
+
+#Why Need pydantic
+#1.Type checking at runtime 
+#2.Data Serialization and Deserialization 
+#3.Data Validation 
+#4.Auto-documentation 
+#5.Field Customization
